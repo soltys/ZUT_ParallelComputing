ddcf457c1fe8dbf6739ef13517527acd520ff75d
Paweł Sołtysiak <soltys@uznam.org>
2014-01-22 20:45:36 +0100

    Added 'all' files

---
+++ all/fft_openmp/build.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+#
+#  Compile the program with GCC.
+#
+SELECTED_SCHEDULE=guided
+gcc -fopenmp fft_openmp.c -lm
+#
+mv a.out fft
+#
+#  Run with 1, 2, and 4 threads.
+#
+echo "Run with 1 thread."
+export OMP_NUM_THREADS=1
+export OMP_SCHEDULE=$SELECTED_SCHEDULE
+./fft > fft_local_gcc_output.txt
+#
+echo "Run with 2 threads."
+export OMP_NUM_THREADS=2
+export OMP_SCHEDULE=$SELECTED_SCHEDULE
+./fft >> fft_local_gcc_output.txt
+#
+echo "Run with 4 threads."
+export OMP_NUM_THREADS=4
+export OMP_SCHEDULE=$SELECTED_SCHEDULE
+./fft >> fft_local_gcc_output.txt
+#
+#  Discard the executable file.
+#
+rm fft
+#
+echo "Program output written to fft_local_gcc_output.txt"
